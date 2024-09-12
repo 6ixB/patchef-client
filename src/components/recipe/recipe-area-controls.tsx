022976280f53f69160402c6747d50baa44a90b1d
6ixB <124480418+6ixB@users.noreply.github.com>
2024-09-12 15:45:24 +0700

    feat: add clear all commands alert dialog

--- src/components/recipe/recipe-area-controls.tsx
+++ src/components/recipe/recipe-area-controls.tsx
@@ -1,20 +1,11 @@
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
+import { AlertDialog, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import { useCommandStore } from '@/hooks/use-command-store';
 import { SquareTerminal, Trash } from 'lucide-react';
+import ClearAlertDialogContent from '@/components/recipe/clear-alert-dialog-content';
 
 const RecipeAreaControls = () => {
-  const { destinationCommands, clearDestinationCommands } = useCommandStore();
+  const { destinationCommands } = useCommandStore();
 
   const isEmpty = destinationCommands.length === 0;
 
@@ -24,28 +15,14 @@ const RecipeAreaControls = () => {
         <AlertDialogTrigger asChild>
           <Button
             disabled={isEmpty}
-            variant="destructive"
+            variant="outline"
             className="flex items-center justify-center gap-x-2"
           >
             <Trash className="size-4" />
             Clear
           </Button>
         </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete all the
-              commands in the recipe.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={clearDestinationCommands}>
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
+        <ClearAlertDialogContent />
       </AlertDialog>
       <Button
         disabled={isEmpty}
