d3230e1b76cd3b54c498c2dfbadf32dda47918a3
6ixB <124480418+6ixB@users.noreply.github.com>
2024-09-12 15:46:28 +0700

    feat: add manage commands dialog

--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -1,8 +1,14 @@
-import { Terminal, Wrench } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import CommandSearch from "@/components/commands/command-search";
+import { Terminal, Wrench } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import CommandSearch from '@/components/commands/command-search';
+import { useCommandStore } from '@/hooks/use-command-store';
+import { Icons } from '@/components/ui/icons';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import ManageDialogContent from '@/components/sidebar/manage-dialog-content';
 
 const SidebarHeader = () => {
+  const { isManaging, setIsManaging } = useCommandStore();
+
   return (
     <div className="w-full px-8 pt-4 pb-4 flex flex-col gap-y-4">
       <div className="flex items-center justify-between">
@@ -10,10 +16,26 @@ const SidebarHeader = () => {
           <Terminal className="size-4" />
           <h1 className="font-medium">Commands</h1>
         </div>
-        <Button className="flex items-center justify-center gap-x-2">
-          <Wrench className="size-4" />
-          Manage
-        </Button>
+
+        <Dialog
+          onOpenChange={(open) => {
+            setIsManaging(!isManaging);
+          }}
+        >
+          <DialogTrigger asChild>
+            <Button className="flex items-center justify-center gap-x-2">
+              {!isManaging ? (
+                <>
+                  <Wrench className="size-4" />
+                  Manage
+                </>
+              ) : (
+                <Icons.spinner className="animate-spin size-4" />
+              )}
+            </Button>
+          </DialogTrigger>
+          <ManageDialogContent />
+        </Dialog>
       </div>
       <CommandSearch />
     </div>
