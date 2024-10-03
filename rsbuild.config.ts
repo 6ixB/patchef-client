f3de35360bb946261ac9ef864f60ea955d744d54
6ixB <124480418+6ixB@users.noreply.github.com>
2024-10-03 22:31:40 +0700

    refactor: remove unnecessary node path resolution for alias

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-// biome-ignore lint/correctness/noNodejsModules: This part of the code is not executed in the browser - MY23-1
-import path from "node:path";
 
 export default defineConfig({
   html: {
@@ -13,7 +11,7 @@ export default defineConfig({
   },
   source: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "./src",
     },
   },
   server: {
