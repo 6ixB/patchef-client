7c60e97e9a2e89912001a740fb6dfa672415c952
6ixB <124480418+6ixB@users.noreply.github.com>
2024-10-07 17:08:20 +0700

    feat: add loader to code markdown when not yet highlighted

--- src/components/markdown/code.tsx
+++ src/components/markdown/code.tsx
@@ -7,6 +7,7 @@ import rehypeStringify from "rehype-stringify";
 import rehypePrettyCode from "rehype-pretty-code";
 import { transformerCopyButton } from "@rehype-pretty/transformers";
 import styles from "@/styles/code.module.css";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 export interface CodeProps {
   codeMarkdown: string;
@@ -54,13 +55,18 @@ const Code = ({ codeMarkdown }: CodeProps) => {
     highlightCode();
   }, [codeMarkdown, theme]);
 
-  return (
+  return codeHtml ? (
     <section
       style={styles}
       className="h-full w-full flex-1 rounded border bg-gray-100 p-4 dark:bg-[#171823]"
       // biome-ignore lint/security/noDangerouslySetInnerHtml: Still debating if this is a good idea - MY23-1
       dangerouslySetInnerHTML={{ __html: codeHtml }}
     />
+  ) : (
+    <section className="flex h-full w-full flex-1 items-center justify-center rounded border bg-gray-100 p-4 dark:bg-[#171823]">
+      {/* I wonder if there's better and dynamic way to render the loader - MY23-1 */}
+      <PropagateLoader size={6} className="size-1" color="#7c3aed" />
+    </section>
   );
 };
 
