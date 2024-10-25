47131379b4105f151b93604ef994c6949015c2f9
6ixB <124480418+6ixB@users.noreply.github.com>
2024-10-25 15:57:19 +0700

    feat(recipes): add auto remove empty recipe when applied

--- src/components/recipe/template/recipe-template-list-item-apply-button.tsx
+++ src/components/recipe/template/recipe-template-list-item-apply-button.tsx
@@ -12,9 +12,12 @@ import { Button } from "@/components/ui/button";
 import { useCommandStore } from "@/hooks/use-command-store";
 import { useRecipeStore } from "@/hooks/use-recipe-store";
 import type { RecipeEntity } from "@/types/recipes/recipe.entity";
+import { useMutation } from "@tanstack/react-query";
 import { ArrowRightToLineIcon } from "lucide-react";
 import { useCallback, useState } from "react";
+import { toast } from "sonner";
 import { v4 as generateUuidV4 } from "uuid";
+import { removeRecipe as removeRecipeApi } from "@/api/recipe.api";
 
 interface RecipeTemplateListItemApplyButtonProps {
   recipe: RecipeEntity;
@@ -25,13 +28,35 @@ const RecipeTemplateListItemApplyButton = ({
 }: RecipeTemplateListItemApplyButtonProps) => {
   const [showDialog, setShowDialog] = useState(false);
   const { destinationCommands, setDestinationCommands } = useCommandStore();
-  const { setActiveRecipe } = useRecipeStore();
+  const { setActiveRecipe, removeRecipe } = useRecipeStore();
+
+  const removeCommandMutation = useMutation({
+    mutationKey: ["remove-recipe", recipe.id],
+    mutationFn: removeRecipeApi,
+  });
 
   const openDialog = useCallback(() => {
     setShowDialog(true);
   }, []);
 
   const applyRecipe = useCallback(() => {
+    if (!recipe.commands) {
+      const promise = () => new Promise<void>((resolve) => setTimeout(async () => {
+        await removeCommandMutation.mutateAsync(recipe);
+        removeRecipe(recipe.id);
+        resolve();
+      }, 3000));
+
+      toast.promise(promise, {
+        loading: 'Recipe has no commands, removing recipe...',
+        success: () => {
+          return `Recipe removed successfully - ${recipe.name}`;
+        },
+        error: 'Error',
+      });
+      return;
+    }
+
     /* 
       To prevent drag and drop issues, we need to create a new array of commands,
       with new ids for each command. Because the drag and drop library uses the id to
